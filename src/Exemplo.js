72d38698774e68f9d40a1c007db7d66456185b2d
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-01 13:35:46 -0300

    Aula do dia 29/02

---
+++ src/Exemplo.js
@@ -0,0 +1,11 @@
+function Exemplo() {
+    return(
+        <div>
+            <h1>
+                Esse é o componente chamado Exemplo.js
+            </h1>
+        </div>
+    )
+
+}
+export default Exemplo;

54f4e4c71737d6ca47994e5d015d7c71c2eb11b7
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-12 10:34:52 -0300

    07/03 Aula

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import { useState } from "react";
+import Nome from "./Components/Nome";
 function App() {
+  const [aluno, setAluno] = useState('Aluno')
+
+  function handleChangeName(name) {
+    setAluno(name)
+  }
   return (
     <div>
-      <h1>
-        Bem vindo ao meu projeto!
-      </h1>
+      <h1>Minha página Web!</h1>
+      <h2> Olá {aluno} </h2>
+      <button onClick={()=>handleChangeName('César') }>
+        Mudar nome
+      </button>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Exemplo from "./Exemplo";
+import Exemplo from "./Components/Exemplo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
